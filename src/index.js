e8b02a78b682885c82fe569a68aaceacb0f95f4f
grassleaf99 <65818460+grassleaf99@users.noreply.github.com>
2021-07-16 23:18:27 +0700

    more about functional component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react'; // dung React de tao component
-import ReactDom from 'react-dom';
+import React from "react"; // dung React de tao component
+import ReactDom from "react-dom";
 
 // function co ten ham ma chu cai dau duoc viet hoa de chi dinh do la functional component
-function Greeting(){
-    return <h2>This is first component</h2>
+function Greeting() {
+  return <h2>This is functional component</h2>;
 }
 
-// ReactDom.render nhan 2 doi so: components se duoc render va noi se render components 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+const Hi = () => {
+  // ham React.createElement nhan 3 doi so: element tra ve, props, children (cai se duoc render trong element tra ve)
+  return React.createElement("h1", {}, "This is also a functional component");
+  //return React.createElement("div", {}, React.createElement("h1", {}, "This is also a functional component"));
+};
+
+// ReactDom.render nhan 2 doi so: components se duoc render va noi se render components
+ReactDom.render(
+  <div>
+    <Greeting /> <Hi />
+  </div>,
+  document.getElementById("root")
+);
 //ReactDom.render(<Greeting></Greeting>, document.getElementById('root'));
