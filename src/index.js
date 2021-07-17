ae771a58dda10e8a814c6e52de8fd693547b1713
grassleaf99 <65818460+grassleaf99@users.noreply.github.com>
2021-07-17 22:13:20 +0700

    basic event (because it doesn't use state)

--- src/index.js
+++ src/index.js
@@ -31,11 +31,29 @@ function BookList() {
 
 const Book = (props) => {
   const { title, imageSrc } = props;
+  const clickHandler = () => {
+    alert("You just click the button");
+  };
+  const complexClickHandler = (title) => {
+    alert("Book's title is " + title);
+  };
   return (
     <article className="book-item">
       <p style={{ fontSize: "1.5rem" }}>This is a book</p>
       <h4>{title}</h4>
       <img src={imageSrc} alt="Image of Book" />
+      <br />
+      <button className="btn-click" type="button" onClick={clickHandler}>
+        Click me
+      </button>
+      <br />
+      <button
+        className="btn-click"
+        type="button"
+        onClick={() => complexClickHandler(title)}
+      >
+        Show book's title
+      </button>
     </article>
   );
 };
